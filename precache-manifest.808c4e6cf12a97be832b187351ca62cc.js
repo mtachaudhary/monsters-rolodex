adcea32f52a583dad5e4e8969fef1afc1e1fe6c8
mtachaudhary <tayyab.chaudhary786@gmail.com>
2020-02-05 16:40:27 +0500

    Updates

---
+++ precache-manifest.808c4e6cf12a97be832b187351ca62cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fd27984a48faaffefc73ae64c748ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "81ec53f74bed01db6c1c",
+    "url": "/monsters-rolodex/static/css/main.ccdf0ac4.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81ec53f74bed01db6c1c",
+    "url": "/monsters-rolodex/static/js/main.0e5d539b.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
